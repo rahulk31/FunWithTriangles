ece83aa1734b1e53ee472674314ff1d63f57b78d
Rahul <devtuber@Rahuls-MacBook.local>
2021-11-17 21:18:08 +0530

    completed quiz

---
+++ js/quiz.js
@@ -0,0 +1,34 @@
+const quizForm = document.querySelector('.quiz-form');
+const btn = document.querySelector('.btn');
+const output = document.querySelector('.output');
+
+const correctAnswers = ['90°', 'right-angled'];
+
+const clickHandler = () => {
+    let score = 0;
+    let index = 0;
+    const formResults = new FormData(quizForm);
+    for(let entry of formResults.values()) {
+        if(entry === correctAnswers[index]) {
+            score++;
+            index++;
+        } else {
+            index++;
+        }
+    }
+    if(score > 0) {
+        output.textContent = `Your score is ${score}`;
+        output.classList.remove('hidden');
+        output.classList.remove('wrong');
+        output.classList.add('right');      
+    } else {
+        output.textContent = `Your score is ${score}`;
+        output.classList.remove('hidden');
+        output.classList.remove('right');
+        output.classList.add('wrong');        
+    }
+
+
+}
+
+btn.addEventListener('click', clickHandler);
