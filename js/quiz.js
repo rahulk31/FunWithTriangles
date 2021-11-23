18b10fe82a1d308ebc67440d6af61c278b0f44ab
Rahul <rahulsingh3118.rs@gmail.com>
2021-11-23 19:26:10 +0530

    updated questions in QUIZ and fixed broken nav links

--- js/quiz.js
+++ js/quiz.js
@@ -2,7 +2,7 @@ const quizForm = document.querySelector('.quiz-form');
 const btn = document.querySelector('.btn');
 const output = document.querySelector('.output');
 
-const correctAnswers = ['90°', 'right-angled'];
+const correctAnswers = ['90°', 'right-angled', '60°', 'Scalene', 'Yes'];
 
 const clickHandler = () => {
     let score = 0;
